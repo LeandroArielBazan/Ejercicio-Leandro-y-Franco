ff6cd8a1ce00f650d58ed34aaa54d6b02fae0452
Leandro y Franco <leanbazan99@gmail.com>
2023-04-04 11:11:29 -0300

    ejercicio corregido

--- algoritmo.js
+++ algoritmo.js
@@ -1,9 +1,9 @@
-let InicialPais = prompt ("Ingresar inicial de un país")
-let MensajeChat = prompt ("Mensaje de chat para una reuinión")
-let PesoBotella = prompt ("Peso de una botella en litros")
-let NombreCentro = prompt ("Nombre del centro")
-let ContraseñaYoutube = prompt ("1!-_.-,asf_flaxse")
-let NumeroCelular = prompt ("Numero de celular")
-let NombrePostre = prompt ("Nombre de algún postre")
-let KeyProducto = prompt ("Key de algún producto comprado online")
-
+let inicialPais = "f";
+let mensajeChat = "entrar";
+let pesoBotella = 2;
+let nombreCentro = "cfp27";
+let contrasenaYoutube = "asjks'asadsd";
+let numeroCelular = "11759849904";
+let kombrePostre = "helado";
+let keyProducto = "123-U-awell";
+let estadoServicio = true;
