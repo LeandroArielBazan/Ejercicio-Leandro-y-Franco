454032619aa7ab1d30712fd339c850fd9f484f10
Leandro y Franco <leanbazan99@gmail.com>
2023-04-04 10:30:20 -0300

    ejercicio variables

---
+++ algoritmo.js
@@ -0,0 +1,9 @@
+let InicialPais = prompt ("Ingresar inicial de un país")
+let MensajeChat = prompt ("Mensaje de chat para una reuinión")
+let PesoBotella = prompt ("Peso de una botella en litros")
+let NombreCentro = prompt ("Nombre del centro")
+let ContraseñaYoutube = prompt ("1!-_.-,asf_flaxse")
+let NumeroCelular = prompt ("Numero de celular")
+let NombrePostre = prompt ("Nombre de algún postre")
+let KeyProducto = prompt ("Key de algún producto comprado online")
+
